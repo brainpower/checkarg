48ba31303f5df4a95496894d1492deb8c3591cdc
brainpower <brainpower@gulli.com>
2014-04-17 00:15:42 +0200

    * add makefile and rename tests dir

--- cpp/tests/run_tests.sh
+++ cpp/tests/run_tests.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 printf " * Testing --help ... "
-if ./test_args --help >/dev/null; then
+if tests/test_args --help >/dev/null; then
 	printf "[ok]\n"
 else
 	printf "[fail]\n"
 fi
 
 printf " * Testing missing value return code ... "
-./test_args -i 2>/dev/null
+tests/test_args -i 2>/dev/null
 if [ $? = 4 ]; then
 	printf "[ok]\n"
 else
