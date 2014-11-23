e04540e91d8cde3f74bed1cb7ef3d507fad56c61
brainpower <brainpower@gulli.com>
2014-11-24 00:21:25 +0100

    cpp: fix typo add aditional text from readme

--- cpp/src/checkarg++.cpp
+++ cpp/src/checkarg++.cpp
@@ -34,16 +34,42 @@ using namespace std;
  * Therefore it pulls no 'external' dependencies and relies completely on the STL.
  *
  * That way it's pretty easy to integrate into an existing project,
- * because it's non-intrusive, you dont need to 'build your project arount it'.
+ * because it's non-intrusive, you dont need to 'build your project around it'.
  *
  * Commandline options use the common so-called GNU style,
  * e.g. use double dashes and look something like these: <br>
- *  -\-option[=value] <br>
- *  -\-option [value] <br>
+ * 
+ *   program --option=value --quiet --option2 <value><br>
+ * 
  * Additionally, abreviations of those in so-called POSIX style can be added.
  * They look like this: <br>
- *  -o [value]
- * I'll call them long and short options, respectively.
+ * 
+ *   program -o <value> -q -f <value>
+ * 
+ * And they can be combined:
+ * 
+ *   program -o <value> -qf <value>
+ *   program -o <value> -qf<value>
+ * 
+ * Note that above commands are equivalent.
+ * This means after a value type option is found in a grouped option
+ * the remainder is always considered to be the value,
+ * even if you meant it to be an option.<br>
+ * So these commands are not the same:
+ * 
+ *   program -o <value> -qfi other_file
+ *   program -o <value> -q -f other_file -i
+ * 
+ * In the first command, 'i' is considered the value of -f,
+ * other_file a positional argument, not the value of either -f or -i.
+ * 
+ * In the docs I'll call the GNU style and POSIX style options "long" and "short" options, respectively.
+ * 
+ * Old style or traditional style options are *not* supported and I don't plan to do so.<br>
+ * You may convince me otherwise, though.<br>
+ * Old style options (like tar supports) look like this:
+ *   tar xfL <value_for_f> <value_for_L>
+ * 
  *
  * Using CheckArg as a developer can be divided in two phases, pre-parse() and post-parse().
  * In the pre-parse phase, you'll define your commandline options using the add() members.
