e47969e049d9e1dfb8b12c5584815e97608d3aaa
brainpower <brainpower@mailbox.org>
2015-07-12 01:51:09 +0200

    bash: rewrite bash impl to keep it's api somewhat consistent with other implementations

--- bash/checkarg.sh
+++ bash/checkarg.sh
@@ -45,23 +45,139 @@
 #
 ##
 
-if [ -z "$CHECKARG_SH" ]; then # include guard
+if [ -z "$CHECKARG_SH" ]; then # "include guard"
   CHECKARG_SH=1
 
+_revert_nounset=0
+if set -o | grep unset | grep -q off; then
+	set -o nounset
+	_revert_nounset=1
+fi
+
 ##
-# initialize global vars used by checkarg
+# initialize public global vars used by checkarg
 ##
+# version string
+readonly CA_VER_STRING="1.2.0"
+
 declare -a  checkarg_pos_args;
-declare -A _checkarg_valid_args;
-declare -A _checkarg_autohelp;
-_checkarg_has_val=''
-_checkarg_autohelp_descr=''
-_checkarg_autohelp_appendix=''
-_checkarg_autohelp_posargname=''
+
+##
+# initialize "private" vars
+##
+declare -A _checkarg_args;
+declare -A _checkarg_args_cb;
+declare -A _checkarg_args_help;
+declare -A _checkarg_args_values;
+declare -A _checkarg_short_long;
+
+_checkarg_appname=''
+_checkarg_descr=''
+_checkarg_appendix=''
+_checkarg_pos_arg_sep=0
+_checkarg_usage_line=''
+_checkarg_posarg_descr=''
+_checkarg_posarg_usage=''
+
+_checkarg_next_is_val_of=''
 
 ##
 # "public" checkarg functions
 ##
+
+##
+# initializes checkarg
+# $1: appname
+# $2: description (opt.)
+# $3: appendix (opt.)
+##
+function checkarg_init(){
+	if [[ -z "$1" ]]; then
+		return 1
+	else
+
+		_checkarg_appname="$1"
+		if [[ -n "$2" ]]; then
+			_checkarg_descr="$2"
+		fi
+		if [[ -n "$3" ]]; then
+			_checkarg_appendix="$3"
+		fi
+	fi
+}
+
+##
+# $1: sopt
+# $2: lopt
+# $3: help
+# $4: has_val (0 no, 1 yes)
+##
+function checkarg_add(){
+	if [[ -z "$1" ]] || [[ -z "$2" ]] || [[ -z $3 ]]; then
+		return 1
+	else
+		eval "_checkarg_short_long[$1]='$2'"
+		eval "_checkarg_args[$2]='${4:-0}'"
+		eval "_checkarg_args_help[$2]='$3'"
+	fi
+}
+
+##
+# $1: sopt
+# $2: lopt
+# $3: command name
+# $4: help
+# $5: has_val (0 no, 1 yes)
+##
+function checkarg_add_cb(){
+	if [[ -z "$1" ]] || [[ -z "$2" ]] || [[ -z "$3" ]] || [[ -z $4 ]]; then
+		return 1
+	else
+		eval "_checkarg_short_long[$1]='$2'"
+		eval "_checkarg_args[$2]='${5:-0}'"
+		eval "_checkarg_args_help[$2]='$4'"
+		eval "_checkarg_args_cb[$2]='$3'"
+	fi
+}
+
+##
+# $1: lopt
+# $2: help
+# $3: has_val (opt.)
+##
+function checkarg_add_long(){
+	if [[ -z "$1" ]] || [[ -z "$2" ]]; then
+		return 1
+	fi
+
+	eval "_checkarg_args[$1]='${3:-0}'"
+	eval "_checkarg_args_help[$1]='$2'"
+}
+
+##
+# $1: lopt
+# $2: command name
+# $3: help
+# $4: has_val (opt.)
+##
+function checkarg_add_long_cb(){
+	if [[ -z "$1" ]] || [[ -z "$2" ]] || [[ -z $3 ]]; then
+		return 1
+	fi
+
+	eval "_checkarg_args[$1]='${4:-0}'"
+	eval "_checkarg_args_help[$1]='$3'"
+	eval "_checkarg_args_cb[$1]='$2'"
+}
+##
+# this adds the arguments '-h' and '--help' with
+# an automatically generated help-message.
+##
+
+function checkarg_add_autohelp(){
+	checkarg_add_cb 'h' "help" "_checkarg_show_help" "show this help message and exit"
+}
+
 
 ##
 # Calling this will do the parsing and checking.
@@ -73,166 +189,265 @@ _checkarg_autohelp_posargname=''
 # of valid arguments before calling this.
 # Preferably using the checkarg_add* functions.
 ##
-function checkarg(){
+function checkarg_parse(){
+	local ret=0
+
 	for arg in "$@"; do
-		_checkarg_arg "$arg" || return 1
+		if ! _checkarg_arg "$arg"; then
+			ret="$?"
+			break;
+		fi
 	done
-}
 
-##
-# This adds a command line argument and the code to be evaluated,
-# if it's found, to the list of valid arguments.
-# You can also pass it a short description of the argument,
-# which will be used by 'checkarg_autohelp'.
-# This description is optional, but note that autohelp
-# wont print any decent description if you dont specify one.
-#
-# Of course long arguments can also be value-type arguments.
-# Even arguments of the form --long=value are supported
-# You could add all four types with this function,
-# but I'd recommend to use the convenience-functions below wherever possible.
-#
-# Examples: (for simple, long, value-type and long value-type argument)
-#    checkarg_add 'v' 'print_version=1' "Print programm's version and exit."
-#    checkarg_add '-version' 'print_version=1' "Print programm's version and exit."
-#    checkarg_add 'i' '_checkarg_has_val=input_file' 'The file to read from.'
-#    checkarg_add '-input' '_checkarg_has_val=input_file' 'The file to read from.'
-##
-function checkarg_add(){
-	if [ -n "$2" ]; then eval "_checkarg_valid_args[$1]='$2'"
-	else                 eval "_checkarg_valid_args[$1]=''"
+	if [[ -n "$_checkarg_next_is_val_of" ]]; then
+		return 4
 	fi
-	if [ -n "$3" ]; then eval "_checkarg_autohelp[$1]='$3'"
-	else                 eval "_checkarg_autohelp[$1]=''"
+
+	# TODO: cleanup?
+}
+
+##
+# auto-help related setters
+##
+
+# $1: description of the positional args
+# $2: text to be appended to usage line for positional args
+function checkarg_set_posarg_help(){
+	_checkarg_posarg_descr="$1"
+	_checkarg_posarg_usage="$2"
+}
+
+# $1: set the usage line used in autohelp
+function checkarg_set_usage_line(){
+	_checkarg_usage_line="$1"
+}
+
+# $1: long opt name
+# returns 0 if opt was given, 1 if not
+function checkarg_isset(){
+	[[ -n "${_checkarg_valid_args[$1]}" ]]
+}
+
+# $1: long opt name
+# prints argument value
+function checkarg_value(){
+	if [[ -n "${_checkarg_valid_args[$1]}" ]]; then
+		printf "${_checkarg_valid_args[$1]}"
+	else
+		return 1
 	fi
 }
 
-##
-# This is a convenience function for adding value-type arguments.
-#
-# Example:
-#    checkarg_add_val 'i' 'input_file' 'The file to read from.'
-##
-
-function checkarg_add_val(){
-	checkarg_add "$1" "_checkarg_has_val='$2'" "$3"
-}
-
-##
-# This is a convenience function for adding long arguments.
-#
-# Example:
-#    checkarg_add_long 'version' 'print_version=1' "Print programm's version and exit."
-##
-
-function checkarg_add_long(){
-	checkarg_add "-$1" "$2" "$3"
-}
-
-##
-# This is a convenience function for adding long value-type arguments.
-#
-# Example:
-#    checkarg_add_long_val 'input' 'input_file' 'The file to read from.'
-##
-
-function checkarg_add_long_val(){
-	checkarg_add_long "$1" "_checkarg_has_val='$2'" "$3"
-}
-
-##
-# this adds the arguments '-h' and '--help' with
-# an automatically generated help-message.
-# It takes the name of the programm.
-#
-# Example:
-#    checkarg_add_autohelp "mySuperScript.sh"
-##
-
-function checkarg_add_autohelp(){
-	checkarg_add_long 'help' "_checkarg_show_help '$1'" "show this help message and exit"
-	checkarg_add      'h'    "_checkarg_show_help '$1'" "show this help message and exit"
-	_checkarg_autohelp_descr="$2"
-	_checkarg_autohelp_appendix="$3"
-	_checkarg_autohelp_posargname="$4"
+# $1: errno
+# prints error message
+function checkarg_strerr(){
+	local ret=1
+	case "$1" in
+		0|CA_ALLOK)
+			printf "Everything is fine"
+			ret=0
+			;;
+		1|CA_ERROR)
+			printf "An error occurred"
+			ret=1
+			;;
+		2|CA_INVARG)
+			printf "Unknown command line argument"
+			ret=2
+			;;
+		3|CA_INVVAL)
+			printf "Value given to non-value argument"
+			ret=3
+			;;
+		4|CA_MISSVAL)
+			printf "Missing value of argument"
+			ret=4
+			;;
+		5|CA_CALLBACK)
+			printf "Callback returned with error code"
+			ret=5
+			;;
+		*)
+			printf "An unknown error occurred"
+			ret=-1
+			;;
+	esac
+	return "$ret"
 }
 
 ##
 # "private" checkarg functions
 ##
 
+# $1: arg
 function _checkarg_arg(){
-	if [ -n "$_checkarg_has_val" ]; then
-		_checkarg_eval_has_val "$1"
-	elif [ "${1:0:1}" = "-" ]; then # check if it is an argument or just weirdness
-		if [ "${1:1:1}" = "-" ]; then # it's a long arg
-			_checkarg_long_arg "$1"
-		else # it's a short one
-			_checkarg_short_arg "$1"
+	local ret=0
+
+	# if the separator '--' was given, all following args are posargs,
+	# so skip any parsing and add them directly to posargs list.
+	#
+	if [[ "$_checkarg_pos_arg_sep" == 0 ]]; then
+
+		if [[ -n "$_checkarg_next_is_val_of" ]]; then
+			# got a value of an option?
+
+			eval "_checkarg_args_values[$_checkarg_next_is_val_of]='$1'"
+
+			if [[ -n "${_checkarg_args_cb[$_checkarg_next_is_val_of]}" ]]; then
+				${_checkarg_args_cb[$1]} "$_checkarg_next_is_val_of" "$1"
+				ret=$?
+			fi
+
+			_checkarg_next_is_val_of=''
+
+			return $ret
 		fi
-	else
-		checkarg_pos_args+=("$1")
-		return 0 # ignore everything unknown
+
+		if [[ "${1:0:1}" = "-" ]]; then # it's an option
+			if [[ "${1:1:1}" = "-" ]]; then # it's a long option
+				if [[ "$1" = "--" ]]; then # it's the posarg sep: '--'
+					_checkarg_pos_arg_sep=1
+					return 0
+				fi
+
+				_checkarg_arg_long "${1:2}" # omit dashes
+				return $?
+			fi
+
+			_checkarg_arg_short "${1:1}" # omit dash
+			return $?
+		fi
 	fi
+
+	# if it's fallen through to here, it's a positional arg
+	checkarg_pos_args+=("$1")
+	return 0
 }
 
-function _checkarg_eval_has_val(){
-	eval "$_checkarg_has_val=\"$1\""
-	_checkarg_has_val=''
-}
+function _checkarg_arg_short(){
+	local len=${#1} # length of the arg
 
-function _checkarg_short_arg(){
-	local str="${1#?}"
-	local len=${#str} # length of the string, minus the dash
-
-	for i in $(eval echo "{1..$len}") ; do
+	for i in $(eval echo "{0..$(( len - 1))}") ; do
 		local arg="${1:$i:1}"
-		local ev="${_checkarg_valid_args[$arg]}"
-		if [ -n "${ev}" ]; then
-			eval "${ev}"
+		local lopt="${_checkarg_short_long[$arg]}"
 
-			if [ -n "$_checkarg_has_val" ] && [ ! $i = $len ]; then
-			#  if arg is of value-type and val-type arg is not last one in arg-group -> remainder is value
-				(( i++ ))
-				_checkarg_eval_has_val "${1:$i}" # set the var specified in has_val to this string
-				return 0 # stop parsing
+		if [[ -n "$lopt" ]]; then
+			if [[ "${_checkarg_args[$lopt]}" -gt 0 ]]; then
+				if [[ -n "${1:$(( i+1 ))}" ]]; then
+					_checkarg_args_values="${1:$(( i+1 ))}"
+					if [[ "${_checkarg_args[$lopt]}" = 0 ]]; then
+						if [[ -n "${_checkarg_args_cb[$lopt]}" ]]; then
+							${_checkarg_args_cb[$lopt]} "$lopt" "$value"
+							return $?
+						else
+							return 0
+						fi
+					fi
+				else
+					_checkarg_next_is_val_of="$lopt"
+				fi
+				return 0
+			else
+				_checkarg_args_values="x" # mark non-value opt as seen
+
+				if [[ "${_checkarg_args[$lopt]}" = 0 ]]; then
+					if [[ -n "${_checkarg_args_cb[$lopt]}" ]]; then
+						${_checkarg_args_cb[$lopt]} "$lopt" ""
+						return $?
+					else
+						return 0
+					fi
+				fi
+				return 0
+
 			fi
 		else
-			echo "Error: Unknown short command line argument: -${1:$i:1}" >&2
-			return 2
+			return 2 # INVARG
 		fi
 	done
+	return 0
 }
 
-function _checkarg_long_arg(){
+# $1: long opt without dashes
+function _checkarg_arg_long(){
 	# if theres an '=' in the arg, check the first part
-	local raw="${1:1}"
-	local arg="${raw%%=*}"
+	local arg="${1%%=*}"
+	local value="${1#*=}"
 
-	if [ -n "${_checkarg_valid_args[$arg]}" ]; then
-		eval "${_checkarg_valid_args[$arg]}"
-		if [ -n "$_checkarg_has_val" ] && [ ! "$arg" = "$raw" ]; then
-			# if arg is of value-type and has an '=' in it -> remainder is value
-			_checkarg_eval_has_val "${raw#*=}" # set the var specified in has_val to this string
+	if [[ -n "${_checkarg_args[$arg]}" ]]; then
+		if [[ "${_checkarg_args[$arg]}" -gt 0 ]] && [[ "$arg" != "$value" ]]; then
+			eval "_checkarg_args_values[$arg]='$value'"
+
+		elif [[ "${_checkarg_args[$arg]}" -gt 0 ]]; then
+			_checkarg_next_is_val_of="$arg"
+
+		elif [[ "$arg" != "$value" ]]; then
+			# value given to non-value option
+			return 3
+		else
+			_checkarg_args_values="x" # mark non-value option as seen
 		fi
+
+		if [[ "${_checkarg_args[$arg]}" = 0 ]] || [[ "$arg" != "$value" ]]; then
+			${_checkarg_args_cb[$1]} "$_checkarg_next_is_val_of" "$1"
+			return $?
+		fi
+		return 0
+
 	else
-		echo "Error: Unknown long command line argument: $1" >&2
+		#echo "Error: Unknown long command line argument: $1" >&2
 		return 2
 	fi
 }
 
 function _checkarg_show_help(){
-	echo "Usage: $1 [Options] $_checkarg_autohelp_posargname"
-	echo ""
-	echo "$_checkarg_autohelp_descr"
-	echo ""
-	echo "Options:"
-	for key in ${!_checkarg_valid_args[@]}; do
-		echo -e "   -$key   \t${_checkarg_autohelp[$key]}"
+	local space=0
+
+	for key in ${!_checkarg_args[@]}; do
+		space="$(( space > ${#key} ? space : ${#key} ))"
 	done
-	echo "$_checkarg_autohelp_appendix"
+
+	printf "Usage: %s [Options] %s\n\n" "$_checkarg_appname" "$_checkarg_posarg_usage"
+
+	printf "$_checkarg_descr\n\n"
+
+	printf "Options:\n"
+
+	for key in ${!_checkarg_args[@]}; do
+
+		sopt="$(_checkarg_find_sopt "$key")"
+		if [[ -z "$sopt" ]]; then
+			printf "       "
+		else
+			printf "   -%s, " "$sopt"
+		fi
+
+		printf -- "--%s%*s  %s\n" "$key" "$(( space - ${#key} ))" "" "${_checkarg_args_help[$key]}"
+	done
+
+	if [[ -n "$_checkarg_posarg_descr" ]]; then
+		printf "\nPositional Argumments:\n%s\n" "$_checkarg_posarg_descr"
+	fi
+
+	if [[ -n "$_checkarg_appendix" ]]; then
+		printf "\n%s\n" "$_checkarg_appendix"
+	fi
 
 	exit 0
 }
+
+function _checkarg_find_sopt(){
+	for key in "${!_checkarg_short_long[@]}"; do
+		if [[ "${_checkarg_short_long[$key]}" == "$1" ]]; then
+			printf -- "$key"
+			return
+		fi
+	done
+	return 1
+}
+
+if [[ $_revert_nounset -gt 0 ]]; then
+	set +o nounset
+fi
 
 fi # CHECKARG_SH
