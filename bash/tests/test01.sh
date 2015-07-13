6733bd170a7676e2e3fe48839932cacfbe8a835f
brainpower <brainpower@mailbox.org>
2015-07-13 23:58:20 +0200

    bash: fix test01

--- bash/tests/test01.sh
+++ bash/tests/test01.sh
@@ -4,10 +4,12 @@ script_dir="$(dirname $(readlink -f "${0}"))"
 
 source "${script_dir}/../checkarg.sh"
 
-checkarg_init "test01" "checkarg test #1"
-checkarg_add "d" "debug" "enable debug messages"
-checkarg_add "o" "output" "output file" 1
-checkarg_add_long "long-only" "option only long"
+checkarg_init "test01"
 checkarg_add_autohelp
+checkarg_add "i" "input" "file to read from" 1
+#checkarg_add "d" "debug" "enable debug messages"
+#checkarg_add "o" "output" "output file" 1
+#checkarg_add_long "long-only" "option only long"
 
 checkarg_parse "$@"
+exit "$?"
