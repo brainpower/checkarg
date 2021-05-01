b88faee778a038db937fd0c6fb51f43258e51418
brainpower <brainpower@mailbox.org>
2021-05-01 20:54:53 +0200

    c: first 2 working tests ported from cpp/tests/

---
+++ c/tests/01_constructors.cpp
@@ -0,0 +1,77 @@
+#include "test.hpp"
+
+
+TEST_CASE("constructor: simple", "[constructor]"){
+  int argc = 1;
+  const char* argv[] = {
+    "/usr/bin/name",
+    NULL,
+  };
+
+
+  CheckArgUPtr ca(checkarg_new(argc, (char**)argv, "name", NULL, NULL), &checkarg_free);
+
+  auto ret = checkarg_parse(ca.get());
+  REQUIRE(ret == CA_ALLOK);
+
+
+  SECTION("call name") {
+    auto ret2 = checkarg_argv0(ca.get());
+
+    REQUIRE(ret2 == string("/usr/bin/name"));
+  }
+
+  SECTION("appname") {
+    auto usage = checkarg_usage(ca.get());
+
+    REQUIRE(usage == string("Usage: name [options]"));
+  }
+}
+
+TEST_CASE("constructor: simple (vector)", "[constructor]"){
+  const vector<string> argv = {
+    "/usr/bin/name",
+  };
+
+  CheckArgUPtr ca(checkarg_new(argv.size(), (char**)argv.data(), "name", NULL, NULL), &checkarg_free);
+
+  auto ret = checkarg_parse(ca.get());
+  REQUIRE(ret == CA_ALLOK);
+
+  SECTION("call name") {
+    auto ret2 = checkarg_argv0(ca.get());
+    REQUIRE(ret2 == string("/usr/bin/name"));
+  }
+
+  SECTION("appname") {
+    auto usage = unique_ptr<char>(checkarg_usage(ca.get()));
+
+    REQUIRE(usage.get() == string("Usage: name [options]"));
+  }
+}
+
+
+#if 0 // HAS_BASENAME
+TEST_CASE("constructor: app name autodetect", "[constructor]"){
+  const vector<string> argv = {
+    "/usr/bin/name",
+  };
+
+  CheckArg ca(argv);
+
+  auto ret = checkarg_parse();
+  REQUIRE(ret == CA_ALLOK);
+
+  SECTION("call name") {
+    auto ret2 = checkarg_argv0();
+    REQUIRE(ret2 == "/usr/bin/name");
+  }
+
+  SECTION("appname") {
+    auto usage = checkarg_usage();
+
+    REQUIRE(usage == "Usage: name [options]");
+  }
+}
+#endif
+
---
+++ c/tests/02_basic_parsing.cpp
@@ -0,0 +1,117 @@
+#include "test.hpp"
+
+// FIXME: remove these and actually port the features from C++ variant
+enum {
+  CA_VT_NONE = 0,
+  CA_VT_REQUIRED = 1,
+};
+enum {
+  CA_INVOPT = CA_INVARG,
+};
+
+
+
+
+bool cb_ran = false;
+string out;
+
+int
+callback_help(CheckArg *ca, const char*, const char*) {
+  cb_ran = true;
+  unique_ptr<char> xout(checkarg_autohelp(ca));
+  out = xout.get();
+  return CA_ALLOK;
+}
+
+
+TEST_CASE("parsing: help using --help", "[parsing]") {
+  vector<const char*> argv = {
+    "/test01",
+  };
+
+  argv.push_back(GENERATE("--help", "-h"));
+
+  cb_ran = false;
+  out.clear();
+
+  CheckArgUPtr ca(checkarg_new(argv.size(), (char**)argv.data(), "test01", NULL, NULL), &checkarg_free);
+
+  // the default callback calls exit(0), which is bad here
+  // checkarg_add_autohelp();
+
+  // it seems one can also not just pass a lambda to a C function
+  /* checkarg_add_cb(ca.get(), */
+  /*   'h', "help", */
+  /*   [&out,&cb_ran](CheckArg *ca, const char*, const char*) -> int { */
+  /*     cb_ran = true; */
+  /*     unique_ptr<char> xout(checkarg_autohelp(ca)); */
+  /*     out = xout.get(); */
+  /*     return CA_ALLOK; */
+  /*   }, */
+  /*   "show this help message and exit" */
+  /* ); */
+  checkarg_add_cb(ca.get(),
+    'h', "help", &callback_help,
+    "show this help message and exit"
+  );
+  checkarg_add_value(ca.get(), 'i', "input", "file to read from", 1);
+  auto ret = checkarg_parse(ca.get());
+
+  CHECK(ret == CA_ALLOK); // -h ran fine
+  CHECK(cb_ran); // callback actually ran
+  REQUIRE(out ==
+    "Usage: test01 [options]\n"
+    "\n"
+    "Options:\n"
+//    "   -h, --help         show this help message and exit\n"
+//    "   -i, --input=INPUT  file to read from\n"
+    "   -h, --help   show this help message and exit\n"
+    "   -i, --input  file to read from\n"
+  ); // callback had access to CA object
+}
+
+TEST_CASE("parsing: correct option and value", "[parsing]"){
+  const vector<const char*> argv = {
+    "/usr/bin/name",
+    "-i", "input.in",
+  };
+
+  CheckArgUPtr ca(checkarg_new(argv.size(), (char**)argv.data(), "test01", NULL, NULL), &checkarg_free);
+  checkarg_add_value(ca.get(), 'i', "input", "file to read from", CA_VT_REQUIRED);
+
+  auto ret = checkarg_parse(ca.get());
+
+  REQUIRE(ret == CA_ALLOK); // value ok
+  REQUIRE(checkarg_isset(ca.get(), "input")); // option detected
+  REQUIRE(checkarg_value(ca.get(), "input") == string("input.in")); // value stored correctly
+}
+
+
+TEST_CASE("parsing: invalid option", "[parsing]"){
+  const vector<const char*> argv = {
+    "/usr/bin/name",
+    "-x",
+  };
+
+  CheckArgUPtr ca(checkarg_new(argv.size(), (char**)argv.data(), "test01", NULL, NULL), &checkarg_free);
+  checkarg_add_value(ca.get(), 'i', "input", "file to read from", CA_VT_REQUIRED);
+
+  auto ret = checkarg_parse(ca.get());
+
+  REQUIRE(ret == CA_INVOPT); // invalid option
+}
+
+TEST_CASE("parsing: missing value", "[parsing]"){
+  const vector<const char*> argv = {
+    "/usr/bin/name",
+    "-i",
+  };
+
+  CheckArgUPtr ca(checkarg_new(argv.size(), (char**)argv.data(), "test01", NULL, NULL), &checkarg_free);
+  checkarg_add_value(ca.get(), 'i', "input", "file to read from", CA_VT_REQUIRED);
+
+  auto ret = checkarg_parse(ca.get());
+
+  REQUIRE(ret == CA_MISSVAL); // missing value
+}
+
